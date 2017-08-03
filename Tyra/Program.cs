b69f4a7386045ea40164263fa3503e87d8fbc7a0
Adedeji Adeoti <adedejiadeoti@gmail.com>
2017-08-03 14:27:37 +0100

    removed excess spaces

--- Tyra/Program.cs
+++ Tyra/Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Tyra
 {
     internal class Program
     {
-        private const long _batchSize = 10000;
+        private const long _batchSize = 300000;
+
         private static void Main(string[] args)
         {
             Console.WriteLine("..............................................");
@@ -19,6 +22,8 @@ namespace Tyra
             Console.WriteLine("..............................................");
             Console.WriteLine("May I have the file you want to clean?");
             Console.Write("Path to file:");
+
+
             String pathToFile = Console.ReadLine();
             while (string.IsNullOrEmpty(pathToFile))
             {
@@ -26,9 +31,9 @@ namespace Tyra
                 pathToFile = Console.ReadLine();
             }
 
-
-
-
+            // delete any previous file
+            String destination = Path.GetDirectoryName(pathToFile);
+            File.Delete(Path.Combine(destination, "fixed.csv"));
 
             try
             {
@@ -38,39 +43,43 @@ namespace Tyra
                     String line;
 
                     int lineCount = 0;
-                    int batch = 0;
+                    //int batch = 0;
                     while ((line = sr.ReadLine()) != null)
                     {
                         lineCount++;
                         string pattern = @"(\d+)(,)(.+)";
                         foreach (Match m in Regex.Matches(line, pattern))
                         {
-                            Person person = new Person();
-                            person.Msisdn = decimal.Parse(m.Groups[1].Value);
-                            person.Name = m.Groups[3].Value;
-                            String subscriber = string.Format(@"{0},{1}", person.Msisdn, person.Name);
+                            decimal msidsdn = decimal.Parse(m.Groups[1].Value);
+
+                            string name = m.Groups[3].Value
+                                .Replace(",", " ")
+                                .Replace("  ", " ")
+                                .Replace("\"", "");
+                            name = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name);
+
+                            string subscriber = string.Format(@"{0},{1}", msidsdn, name);
                             subscribers.Add(subscriber);
                         }
 
                         if (lineCount >= _batchSize)
                         {
                             String directoryName = Path.GetDirectoryName(pathToFile);
-                            String destinationPath = Path.Combine(directoryName, "fixed_batch_" + ++batch + ".csv");
-                            File.WriteAllLines(destinationPath, subscribers);
+                            String destinationPath = Path.Combine(directoryName, "fixed.csv");
+                            File.AppendAllLines(destinationPath, subscribers);
+                            Console.Write("!");
+
 
                             // reset line count
                             lineCount = 0;
                             subscribers.Clear();
-
                         }
                     }
                     String dirName = Path.GetDirectoryName(pathToFile);
-                    String destPath = Path.Combine(dirName, "fixed_batch_" + ++batch + ".csv");
-                    File.WriteAllLines(destPath, subscribers);
+                    String destPath = Path.Combine(dirName, "fixed.csv");
+                    File.AppendAllLines(destPath, subscribers);
+                    Console.WriteLine();
                     // commit the remainng tofile 
-
-
-
                 }
             }
             catch (Exception exception)
@@ -78,9 +87,11 @@ namespace Tyra
                 Console.WriteLine(exception.Message);
             }
 
-          
+
             // Suspend the screen.
-            Console.ReadLine();
+            Console.WriteLine("Done");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
